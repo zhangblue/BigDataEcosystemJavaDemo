6ea62dc28468e82691c12e9f510e0a83f3fad06a
ZhangDi <zhangd@qzt360.com>
2018-03-02 18:53:08 +0800

    添加kafka demo

---
+++ KafkaDemo/src/main/java/cn/com/kafka/connection/KafkaOffsetResetEnum.java
@@ -0,0 +1,19 @@
+package cn.com.kafka.connection;
+
+public enum KafkaOffsetResetEnum {
+
+  /***
+   * latest: 从最新的offset开始读，之前没读的都不要了
+   * earliest: 从现在最小的offset开始读
+   */
+  LAST("latest"), EARLIEST("earliest"), NONE("none");
+  private final String action;
+
+  KafkaOffsetResetEnum(String action) {
+    this.action = action;
+  }
+
+  public String getAction() {
+    return action;
+  }
+}
